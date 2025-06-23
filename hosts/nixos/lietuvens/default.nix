761f0cd8b1107ffa1cc1d8eb88f334b9a9c2f2d5
mkorje <max@mkor.je>
2025-06-23 14:09:42 +1000

    fix(lietuvens): route metrics

--- hosts/nixos/lietuvens/default.nix
+++ hosts/nixos/lietuvens/default.nix
@@ -15,14 +15,24 @@
   systemd.network.networks."10-enp6s0" = {
     matchConfig.Name = "enp6s0";
     address = [ "172.16.3.100/16" ];
-    gateway = [ "172.16.0.1" ];
+    routes = [
+      {
+        Gateway = "172.16.0.1";
+        Metric = 1000;
+      }
+    ];
     linkConfig.RequiredForOnline = "routable";
   };
 
   systemd.network.networks."10-enp5s0" = {
     matchConfig.Name = "enp5s0";
     address = [ "172.19.0.100/24" ];
-    gateway = [ "172.19.0.1" ];
+    routes = [
+      {
+        Gateway = "172.19.0.1";
+        Metric = 1024;
+      }
+    ];
     linkConfig.RequiredForOnline = "routable";
   };
 
