501bcecb064430e027918b4961f42ba5ccd4e3ed
mkorje <max@mkor.je>
2025-08-18 23:44:21 +1000

    fix(auseklis): fix nvidia config

--- hosts/nixos/common/nvidia.nix
+++ hosts/nixos/common/nvidia.nix
@@ -2,31 +2,15 @@
   hardware.graphics.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia.open = true;
-  nixpkgs.config.cudaSupport = true;
 
   environment.sessionVariables = {
+    NVD_BACKEND = "direct";
     LIBVA_DRIVER_NAME = "nvidia";
-    VDPAU_DRIVER = "nvidia";
-    GBM_BACKEND = "nvidia-drm";
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
   };
 
   allowedUnfreePackages = [
-    # Nvidia
     "nvidia-x11"
     "nvidia-settings"
-
-    # CUDA
-    "cuda_cudart"
-    "cuda_nvcc"
-    "cuda_nvrtc"
-    "cuda_nvml_dev"
-    "cuda_cccl"
-    "libcublas"
-    "libcurand"
-    "libcusparse"
-    "libnvjitlink"
-    "libcufft"
-    "cudnn"
   ];
 }
