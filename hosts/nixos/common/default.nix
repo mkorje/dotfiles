25b292200cf06e5a8abae599c1f16f1ff71abc19
mkorje <max@mkor.je>
2025-08-25 00:05:55 +1000

    feat: switch to quad9 dns

--- hosts/nixos/common/default.nix
+++ hosts/nixos/common/default.nix
@@ -64,10 +64,10 @@
 
   services.resolved.enable = true;
   networking.nameservers = [
-    "1.1.1.1"
-    "1.0.0.1"
-    "2606:4700:4700::1111"
-    "2606:4700:4700::1001"
+    "9.9.9.9"
+    "149.112.112.112"
+    "2620:fe::fe"
+    "2620:fe::9"
   ];
 
   security.sudo.enable = false;
