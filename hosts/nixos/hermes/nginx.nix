dfc022fb22de5ea87151b2ff0e902bbc06ddd2f5
mkorje <max@mkor.je>
2025-08-25 12:33:59 +1000

    feat(hermes): add security headers

--- hosts/nixos/hermes/nginx.nix
+++ hosts/nixos/hermes/nginx.nix
@@ -11,8 +11,31 @@
     package = pkgs.nginxQuic;
 
     recommendedOptimisation = true;
-    recommendedTlsSettings = true;
     recommendedGzipSettings = true;
+    recommendedBrotliSettings = true;
+    experimentalZstdSettings = true;
+
+    # https://ssl-config.mozilla.org/#server=nginx&config=modern&ocsp=false
+    sslProtocols = "TLSv1.3";
+    commonHttpConfig = ''
+      ssl_ecdh_curve X25519:prime256v1:secp384r1;
+      ssl_prefer_server_ciphers off;
+    '';
+
+    appendHttpConfig = ''
+      map $scheme $hsts_header {
+        https "max-age=31536000; includeSubdomains; preload";
+      }
+      add_header Strict-Transport-Security $hsts_header always;
+      add_header Content-Security-Policy "default-src 'none'; base-uri 'none'; frame-ancestors 'none'; form-action 'none'; trusted-types 'none'; require-trusted-types-for 'script'" always;
+      add_header Permissions-Policy "accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()" always;
+      add_header Referrer-Policy no-referrer;
+      add_header X-Frame-Options DENY always;
+      add_header X-Content-Type-Options nosniff always;
+      add_header Cross-Origin-Resource-Policy same-origin always;
+      add_header Cross-Origin-Embedder-Policy require-corp always;
+      add_header Cross-Origin-Opener-Policy same-origin always;
+    '';
 
     virtualHosts."${domain}" = {
       default = true;
