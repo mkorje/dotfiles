ac40de8dc85a61dde5cae5cb4e6c9bcf65eac519
mkorje <max@mkor.je>
2025-12-23 17:40:28 +1100

    feat: override czkawka pkg with main

--- hosts/home-manager/common/cli/files.nix
+++ hosts/home-manager/common/cli/files.nix
@@ -32,7 +32,29 @@
     poppler-utils
 
     # Cleaners
-    czkawka
+    (pkgs.czkawka.overrideAttrs (_: rec {
+      version = "unstable-2025-12-03";
+      src = pkgs.fetchFromGitHub {
+        owner = "qarmin";
+        repo = "czkawka";
+        rev = "6296b06f3a6798f07f78cd771ed95d75639f7bbb";
+        hash = "sha256-legzp0mmNgKz9dw7rpnkAXXYjW3BFur8e+W7289bHI8=";
+      };
+      cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+        inherit src;
+        hash = "sha256-16swKsTutg4/T24zpj0BwjYBKbeGdf8BGNDZPgE5PwU=";
+      };
+      checkPhase = ''
+        runHook preCheck
+        xvfb-run cargo test -- \
+          --skip common::cache::tests::test_save_and_load_cache_by_path \
+          --skip common::cache::tests::test_save_and_load_cache_by_size \
+          --skip common::cache::tests::test_save_cache_with_json \
+          --skip common::cache::tests::test_save_cache_with_minimum_file_size
+        runHook postCheck
+      '';
+      doInstallCheck = false;
+    }))
     identity
 
     # Fonts
