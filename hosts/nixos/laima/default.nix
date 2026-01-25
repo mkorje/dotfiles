6895a3bf16fdc0c9d9e9447e7e24938d8df3b798
mkorje <max@mkor.je>
2026-01-25 16:15:28 +1100

    fix(laima): ipv4 config

--- hosts/nixos/laima/default.nix
+++ hosts/nixos/laima/default.nix
@@ -23,35 +23,38 @@
       "5.223.68.149/32"
       "2a01:4ff:2f0:393b::1/64"
     ];
-    gateway = [
-      "172.31.1.1"
-      "fe80::1"
+    routes = [
+      { Gateway = "fe80::1"; }
+      {
+        Gateway = "172.31.1.1";
+        GatewayOnLink = true;
+      }
     ];
   };
 
-  # services.nginx.virtualHosts."${domain}" = {
-  #   default = true;
-  #   forceSSL = true;
-  #   useACMEHost = domain;
-  #   quic = true;
-  #   kTLS = true;
-  #   listenAddresses = [
-  #     "139.84.200.35"
-  #     "[2401:c080:2000:1e23::1]"
-  #   ];
-  #   locations."/" = {
-  #     return = "200 '<html><body>It works</body></html>'";
-  #     extraConfig = ''
-  #       default_type text/html;
-  #     '';
-  #   };
-  #   locations."/robots.txt" = {
-  #     extraConfig = ''
-  #       rewrite ^/(.*)  $1;
-  #       return 200 "User-agent: *\nDisallow: /";
-  #     '';
-  #   };
-  # };
+  services.nginx.virtualHosts."mkor.je" = {
+    default = true;
+    onlySSL = true;
+    useACMEHost = "mkor.je";
+    quic = true;
+    kTLS = true;
+    listenAddresses = [
+      "5.223.68.149"
+      "[2a01:4ff:2f0:393b::1]"
+    ];
+    locations."/" = {
+      return = "200 '<html><body>It works</body></html>'";
+      extraConfig = ''
+        default_type text/html;
+      '';
+    };
+    locations."/robots.txt" = {
+      extraConfig = ''
+        rewrite ^/(.*)  $1;
+        return 200 "User-agent: *\nDisallow: /";
+      '';
+    };
+  };
 
   security.acme.certs = {
     "mkor.je".extraDomainNames = [ "*.mkor.je" ];
