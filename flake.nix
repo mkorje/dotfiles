ca4e5c795e8ccabb5e17a1f2c578adbf3c18e010
mkorje <max@mkor.je>
2026-06-12 18:32:12 +1000

    flake: prune multiple home-manager versions in lock file

--- flake.nix
+++ flake.nix
@@ -17,7 +17,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    impermanence.url = "github:nix-community/impermanence";
+    impermanence = {
+      url = "github:nix-community/impermanence";
+      inputs.nixpkgs.follows = "";
+      inputs.home-manager.follows = "";
+    };
 
     lanzaboote = {
       url = "github:nix-community/lanzaboote/v1.0.0";
