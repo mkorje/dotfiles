1c3ec004a2a481722abd47153f08cab10fe23fd3
mkorje <max@mkor.je>
2026-08-28 19:36:12 +1000

    hm: enable cursor explicitly

--- hosts/home-manager/common/wayland/gtk.nix
+++ hosts/home-manager/common/wayland/gtk.nix
@@ -2,6 +2,7 @@
   gtk.enable = true;
   gtk.gtk4.theme = null;
 
+  home.pointerCursor.enable = true;
   home.pointerCursor.gtk.enable = true;
   catppuccin.cursors.enable = true;
   home.pointerCursor.size = 16;
